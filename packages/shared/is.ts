456bdb88f3d17bd11f0f834ce9edd34f783d9758
Onion-L <lixiang020317@gmail.com>
2024-08-19 16:36:53 +0800

    feat(shared): `is` function

--- packages/shared/is.ts
+++ packages/shared/is.ts
@@ -1,1 +1,9 @@
 export const noop = () => {}
+
+export const isString = (params: any) => {
+  return typeof params === 'string'
+}
+
+export const isAllow = (status: PermissionState | undefined): boolean => {
+  return status === 'granted' || status === 'prompt'
+}
